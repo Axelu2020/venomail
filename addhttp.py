bb4ca51c560667eac735a4d4b059a16f956744fa
Axel Abbas <62534624+Axelu2020@users.noreply.github.com>
2021-02-11 19:00:53 +0300

    Delete addhttp.py

--- addhttp.py
+++
@@ -1,5 +0,0 @@
-with open('proxyy.txt') as f:
-    lines = f.readlines()
-new_lines = [''.join(["http://", x.strip(), "\n"]) for x in lines]
-with open('proxy.txt', 'w') as f:
-    f.writelines(new_lines)
